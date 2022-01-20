59d62e7650a30ef818b3db41478e58988c90ad78
NaimeJuliet <naime2008@hotmail.com>
2022-01-20 09:30:18 -0500

    se crea controlador llenado

---
+++ controladorLlenado.js
@@ -0,0 +1,13 @@
+let produtos=[
+    {nombre:"Aguardiente tapa roja",tamano:"1/2 Botella", precio:200000, foto:"img/tapaRoja.png"},
+    {nombre:"Aguardiente tapa azul", tamaño:"1/2 Botella",precio:220000, foto:"img/tapaAzul.png"},
+    {nombre:"Tequila Don Julio", tamaño:"1 Botella grande",precio:300000, foto:"img/donjulio.jpg"},
+    {nombre:"Cerveza Aguila", tamaño:"1 Botella",precio:8000, foto:"img/aguila.jpg"},
+    {nombre:"Cerveza Pilsen", tamaño:"1 Botella",precio:7000, foto:"img/pilsen.png"},
+    {nombre:"Ron Medellin", tamaño:"1/2 Botella",precio:220000, foto:"img/ronmedellin.jpg"},
+    {nombre:"Ron Añejo 200 años Caldas", tamaño:"1/2 Botella",precio:100000, foto:"img/roncaldas.jpg"},
+    {nombre:"Agua", tamaño:"1 Botella",precio:4000, foto:"img/agua.png"},
+    {nombre:"Soda", tamaño:"1 Botella",precio:6000, foto:"img/soda.jpg"},
+    {nombre:"Gatorade", tamaño:"1 Botella",precio:9000, foto:"img/gatorade.jpg"},
+    
+]
